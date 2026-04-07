3bb623f57cfcd497c7d839a91fb0644728a91168
kusachan <3253975221@qq.com>
2026-04-07 20:58:58 +0800

    最终准备

--- src/components/article/ArticleAIChat.vue
+++ src/components/article/ArticleAIChat.vue
@@ -72,7 +72,7 @@
 </template>
 
 <script>
-const BASE_URL = 'http://127.0.0.1:3000';
+const BASE_URL = process.env.VUE_APP_API_BASE_URL || 'http://127.0.0.1:3000';
 
 export default {
   name: 'ArticleAIChat',
--- src/config/base.config.js
+++ src/config/base.config.js
@@ -7,7 +7,7 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 export default {
-  'BASE_URL': 'http://127.0.0.1:3000',
+  'BASE_URL': process.env.VUE_APP_API_BASE_URL || 'http://127.0.0.1:3000',
   'TIMEOUT': 5000,
   'TOKEN_NAME': 'ua_token',
   'PUBKEY_NAME': 'ua_publicKey',
